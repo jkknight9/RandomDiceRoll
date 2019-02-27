63b915cd8eb4a95d851049d5c8bec35590811e62
Jack Knight <jkknight9@gmail.com>
2019-02-27 09:58:38 -0700

    simple ramdom dice app, also shake to randomize

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDicIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
-
-
+    
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        updateDiceImages()
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDicIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDicIndex2])
+    }
+    
 }
 
